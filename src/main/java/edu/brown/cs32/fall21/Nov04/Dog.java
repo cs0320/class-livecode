0206bb28f16cbbf7ef1cc45ade200ba584d2b7cf
Tim Nelson <tbnelson@gmail.com>
2021-11-04 07:47:22 -0400

    Nov 4 setup

---
+++ src/main/java/edu/brown/cs32/fall21/Nov04/Dog.java
@@ -0,0 +1,19 @@
+package edu.brown.cs32.fall21.Nov04;
+
+import java.util.HashSet;
+
+public class Dog extends Animal {
+    final boolean strength;
+    final boolean insolence;
+
+    Dog(String name, boolean strength, boolean insolence) {
+        super(name, new HashSet<>());
+        this.strength = strength;
+        this.insolence = insolence;
+    }
+
+    @Override
+    public String speak() {
+        return "woof!";
+    }
+}
---
+++ src/main/java/edu/brown/cs32/fall21/Nov04/Talkative.java
@@ -0,0 +1,5 @@
+package edu.brown.cs32.fall21.Nov04;
+
+public interface Talkative {
+    String speak();
+}
