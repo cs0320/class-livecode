21e61718cf484d4309054b81efb15a9ea42a914e
Tim Nelson <tn@cs.brown.edu>
2022-09-08 13:53:44 -0400

    fix: better extensibility, generic Student; better error

--- F22/mostCommonGeneric/src/main/java/edu/brown/cs32/livecode/MostCommon.java
+++ F22/mostCommonGeneric/src/main/java/edu/brown/cs32/livecode/MostCommon.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public class MostCommon {
     public static void main(String[] args) {
-        Student tim = new Student(List.of("lecture notes", "email", "email", "family", "email"));
+        Student<String> tim = new Student<>(List.of("lecture notes", "email", "email", "family", "email"));
         System.out.println(tim.mostCommonTodoItem());
     }
 }
--- F22/mostCommonGeneric/src/main/java/edu/brown/cs32/livecode/Student.java
+++ F22/mostCommonGeneric/src/main/java/edu/brown/cs32/livecode/Student.java
@@ -4,25 +4,28 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class Student {
+public class Student<T> {
     /** This student's to-do list */
-    List<String> todos;
-    public Student(List<String> todos) {
+    List<T> todos;
+    public Student(List<T> todos) {
         this.todos = todos;
     }
 
     /** To-do lists might have the same thing listed multiple times. (Tim's certainly does.)
      *  What's the most needed thing---the most common element on the list?
      */
-    public String mostCommonTodoItem() {
-        Map<String, Integer> counts = new HashMap<>();
-        for(String s : this.todos) {
+    public T mostCommonTodoItem() {
+        if(todos.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+        Map<T, Integer> counts = new HashMap<>();
+        for(T s : this.todos) {
             if(!counts.containsKey(s)) counts.put(s, 1);
             else counts.put(s, counts.get(s) + 1);
         }
-        String mostCommonItem = null;
+        T mostCommonItem = null;
         int howCommon = 0;
-        for(String s : counts.keySet()) {
+        for(T s : counts.keySet()) {
             if(counts.get(s) > howCommon) {
                 mostCommonItem = s;
                 howCommon = counts.get(s);
