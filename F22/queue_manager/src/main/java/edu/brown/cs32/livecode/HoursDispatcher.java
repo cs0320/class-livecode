cabc079546317a1ce3fbb22af82379a6031ec0de
Tim Nelson <tn@cs.brown.edu>
2022-10-07 08:28:55 -0400

    in class for oct 6

--- F22/queue_manager/src/main/java/edu/brown/cs32/livecode/HoursDispatcher.java
+++ F22/queue_manager/src/main/java/edu/brown/cs32/livecode/HoursDispatcher.java
@@ -3,8 +3,107 @@ package edu.brown.cs32.livecode;
 import java.util.*;
 
 public class HoursDispatcher {
-    Iterator<Student> queue;
-    Map<TA, Integer> minutesLeft;
+    private final Iterator<Student> queue;
+    private final Map<TA, Integer> minutesLeft;
+
+    // Critique "defensive copy": what could go wrong?
+    //   (any attacker potential remaining? any features we removed that _good_ user may have relied on?)
+
+    // Option: PROXY with an unmodifiable view:
+    private Map<TA, Integer> public_minutes_left = null;
+    public Map<TA, Integer> getMinutesLeft() {
+        if(public_minutes_left == null) {
+            public_minutes_left = Collections.unmodifiableMap(minutesLeft);
+        }
+        return public_minutes_left;
+    }
+
+    /**
+     * Returns a view of all the currently on duty TAs by name
+     */
+    public Collection<String> getOnDutyView() {
+        return new OnDutyTAView();
+    }
+
+
+    // Adapter pattern: proxy that changes the interface to caller
+    //  Here, we narrow the interface provided. We didn't have to use
+    //  a nested class; that just makes it easier. If we used a normal
+    //  class, probably we'd pass minutesLeft to the constructor, just like
+    //  Collections.unmodifiableMap does.
+    class OnDutyTAView implements Collection<String> {
+
+        @Override
+        public int size() {
+            return minutesLeft.size();
+        }
+
+        @Override
+        public boolean isEmpty() {
+            return false;
+        }
+
+        @Override
+        public boolean contains(Object o) {
+            return false;
+        }
+
+        @Override
+        public Iterator<String> iterator() {
+            return minutesLeft.keySet().stream().map(ta -> ta.name).iterator();
+        }
+
+        @Override
+        public Object[] toArray() {
+            return new Object[0];
+        }
+
+        @Override
+        public <T> T[] toArray(T[] a) {
+            return null;
+        }
+
+        @Override
+        public boolean add(String s) {
+            return false;
+        }
+
+        @Override
+        public boolean remove(Object o) {
+            return false;
+        }
+
+        @Override
+        public boolean containsAll(Collection<?> c) {
+            return false;
+        }
+
+        @Override
+        public boolean addAll(Collection<? extends String> c) {
+            return false;
+        }
+
+        @Override
+        public boolean removeAll(Collection<?> c) {
+            return false;
+        }
+
+        @Override
+        public boolean retainAll(Collection<?> c) {
+            return false;
+        }
+
+        @Override
+        public void clear() {
+
+        }
+    }
+
+
+
+
+
+
     String statusMessage;
 
     HoursDispatcher(Iterator<Student> signups, String statusMessage) {
