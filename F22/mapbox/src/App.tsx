213951c39d29c3010cda1ad8afa2d6dc5492250e
Tim Nelson <tn@cs.brown.edu>
2022-10-30 11:44:25 -0400

    adding mapbox preliminary

---
+++ F22/mapbox/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import MapDemo from './MapDemo'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <MapDemo />      
+    </div>
+  );
+}
+
+export default App;
