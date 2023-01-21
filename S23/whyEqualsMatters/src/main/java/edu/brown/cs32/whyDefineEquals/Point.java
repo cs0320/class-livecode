0fcf86a2c6b4c7a3d31914fa7e5ea998a74627e1
Tim Nelson <tbnelson@gmail.com>
2023-01-21 17:10:07 -0500

    add: example re: defining equality

---
+++ S23/whyEqualsMatters/src/main/java/edu/brown/cs32/whyDefineEquals/Point.java
@@ -0,0 +1,17 @@
+package edu.brown.cs32.whyDefineEquals;
+
+/**
+ * A point in 2-dimensional space.
+ */
+public class Point {
+    final double x;
+    final double y;
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double x() { return x; }
+    public double y() { return y; }
+}
