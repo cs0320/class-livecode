57c59689230ce1124d86e1c66dd88e59ce424f20
Tim Nelson <tbnelson@gmail.com>
2024-09-05 14:06:52 -0400

    change from in-class

--- F24/sep05-generics/src/main/java/edu/brown/cs32/livecode/Main.java
+++ F24/sep05-generics/src/main/java/edu/brown/cs32/livecode/Main.java
@@ -7,11 +7,14 @@ import java.util.List;
  */
 public class Main {
     public static void main(String[] args) {
-        Student tim = new Student(
+        Student<String> tim = new Student<>(
                 List.of("lecture notes", "email", "email", "family", "email"));
         String common = tim.mostCommonTodoItem();
         System.out.println(common);
 
+        Student<Integer> nim = new Student<>(
+                List.of(1,2,3)
+        );
         // Should this produce an error? What do you think?
         // String x = null;
     }
