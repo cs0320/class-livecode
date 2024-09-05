486ae3e98ac05d6706b004c8798d28b5096b33de
Tim Nelson <tbnelson@gmail.com>
2024-09-05 11:09:30 -0400

    more cleanup

--- S24/feb06_queue_manager/src/main/java/edu/brown/cs32/livecode/hours/LimitedDispatcher.java
+++
@@ -1,7 +0,0 @@
-package edu.brown.cs32.livecode.hours;
-
-import java.util.Collection;
-
-public interface LimitedDispatcher {
-    Collection<String> getOnDutyView();
-}
--- S24/feb08_nws_api/src/main/java/edu/brown/cs32/nwsapi/datasource/weather/WeatherData.java
+++
@@ -1,9 +0,0 @@
-package edu.brown.cs32.nwsapi.datasource.weather;
-
-/**
- * A record representing a weather-report datum, or rather the
- * information we want to retain from a weather report and provide
- * to our client(s).
- * @param temp_C the temperature in degrees Celsius
- */
-public record WeatherData(double temp_C) {}
