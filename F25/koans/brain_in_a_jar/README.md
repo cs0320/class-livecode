4da64cc5ab85be41f8c1ee6ded91797ee75d459b
Tim Nelson <tbnelson@gmail.com>
2025-09-25 09:41:31 -0400

    add: update sep 25

---
+++ F25/koans/brain_in_a_jar/README.md
@@ -0,0 +1,5 @@
+# 0320 Koan: "How do you know you're not a brain in a jar?"
+ 
+Ask yourself that question. You have the evidence of your senses, and you "perceive" the effects of your actions. But is that enough? 
+
+This isn't only a philosophical question. It's powerful in SWE, too.
---
+++ F25/koans/brain_in_a_jar/src/mocks.ts
@@ -0,0 +1,83 @@
+import z from 'zod'
+
+/* Example finished version (don't spoil the exercise!) */
+
+export const providence_lat = 41.8240;
+export const providence_lon = -71.4128;
+
+interface TemperatureResult {
+    success: boolean,
+    error?: 'fetchPoints' | 'fetchForecast' | 'validatePoints' | 'validateForecast',
+    degrees?: number,
+    scale?: 'F' | 'C'
+}
+
+const pointsResponseSchema = z.object({
+    properties: z.object({forecast: z.string()})})
+const timePeriodSchema = z.object({
+    temperature: z.number(), 
+    temperatureUnit: z.literal('C').or(z.literal('F'))})
+const forecastResponseSchema = z.object({
+    properties: z.object({periods: z.array(timePeriodSchema)})})
+
+
+/** Make an API call to the National Weather Service. I'm leaving out 
+ *  Zod / careful validation here, because that's not the point of this 
+ *  example. The point is that this function _calls a real web api_.
+ */
+async function getRealTemperature(lat: number, lon: number): Promise<TemperatureResult> {
+    // Step 1: ask the NWS for the grid point data about this location
+    const result: Response = await fetch(`https://api.weather.gov/points/${lat},${lon}`)
+    if (!result.ok) return {success: false, error: 'fetchPoints'}
+    const json = await result.json();
+    const validatedJson = await pointsResponseSchema.safeParse(json)
+    if(!validatedJson.success) return {success: false, error: 'validatePoints'}
+    
+    // Step 2: ask the NWS for a week-long forecast for this location
+    const forecastUrl = validatedJson.data.properties.forecast;
+    const forecastResult = await fetch(forecastUrl);
+    if (!forecastResult.ok) return {success: false, error: 'fetchForecast'}
+    const forecastJSON = await forecastResult.json()
+    const validatedForecastJSON = await forecastResponseSchema.safeParse(forecastJSON)
+    if(!validatedForecastJSON.success) return {success: false, error: 'validateForecast'}
+    
+    // This isn't actually right; it'll return the temp
+    // for the _next time period_, not the current temp.
+    // See: https://github.com/tnelson/pvd-weather/blob/main/src/App.tsx
+    const timePeriodsData = validatedForecastJSON.data.properties.periods
+    return {success: true, 
+            degrees: timePeriodsData[0].temperature, 
+            scale: timePeriodsData[0].temperatureUnit} 
+}
+
+/** Let's do something with that temperature */
+async function weatherReportProvidence(source: WeatherSource) {
+    const report = await source(providence_lat, providence_lon)
+    console.log(`It's ${report.degrees} ${report.scale} out!`)
+}
+
+/** Any function with this shape works as a weather source. */
+type WeatherSource = (latarg: number, lonarg: number) => Promise<TemperatureResult>
+
+/** Build a "temperature source" that never actually asks the NWS, and just
+ *  returns the provided constant value. */
+function buildTemperatureMock(temp: number, scale: 'F' | 'C'): WeatherSource {
+    // This builder returns an async function indistinguishable from the real source.
+    // (Note: we need the explicit return type here to get TypeScript to infer...)
+    return async function(lat: number, lon: number): Promise<TemperatureResult> {
+        return {success: true, degrees: temp, scale: scale}
+    }
+}
+
+const alwaysSunny = buildTemperatureMock(75, 'F')
+
+/** Note: don't try this outside of an async function.*/
+async function demo() {
+    // weatherReportProvidence can't tell the difference.
+    console.log(`real data (beware: this might be the same as the mock :-)):`)
+    await weatherReportProvidence(getRealTemperature)
+    console.log(`mock data:`)
+    // for prototyping/testing
+    await weatherReportProvidence(alwaysSunny)
+}
+demo()
