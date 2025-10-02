3abd620ec64eb89b5fca252989fc7deb5e899a5b
Tim Nelson <tbnelson@gmail.com>
2025-10-02 10:11:26 -0400

    fix: cleanup oct 2

--- F25/oct02_exercise/src/App.tsx
+++ F25/oct02_exercise/src/App.tsx
@@ -1,21 +1,11 @@
 import React from 'react';
-import Puzzle from './Puzzle'
 import './App.css';
 
-/**
- * Top-level application component. Shows the question prompt, and then
- * loads the Puzzle component within it. 
- * @returns JSX for the App component
- */
 function App() {
   return (
     <div className="App">
-      <p className="App-header" data-testid="test:header-text">
-        I'm thinking of a function that either accepts or rejects sequences of 3 numbers. 
-        For example, my function returns true on the sequence: 2, 4, 8.
-        Can you figure out what it is? Try it out and see what it returns on your sequences...
-      </p>
-      <Puzzle />      
+      Nothing yet.
+      {/* What components do you want to put here? */}
     </div>
   );
 }
